b8ca1c4e2455b9005c4e60cf640df420949b0c8a
sondos alansi <sondos.alansi@yahoo.com>
2018-10-23 18:07:46 +0000

    automatically backed up by learn

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react' // We need to import react so we can make use of its .component class
 import ReactDOM from 'react-dom'
-export default class RegistrationForm extends React.Component {
+export default class RegistrationForm extends Component {
 
   render(){
     return(
