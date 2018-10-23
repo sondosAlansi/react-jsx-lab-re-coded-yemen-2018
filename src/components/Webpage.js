ea059ac309aaf986dae466c87960269f355b60e5
sondos alansi <sondos.alansi@yahoo.com>
2018-10-23 18:07:54 +0000

    automatically backed up by learn

--- src/components/Webpage.js
+++ src/components/Webpage.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react' // We need to import react so we can make use of its .component class
 import ReactDOM from 'react-dom'
 import FillerText from './FillerText';
-export default class Webpage extends React.Component{
+export default class Webpage extends Component{
   render()
   {
     return(
